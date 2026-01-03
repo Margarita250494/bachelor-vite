a393f9efe9c3f8307982860fcd1db3c609468d8a
margaritaboiko <margo250494@gmail.com>
2026-01-03 16:40:42 +0100

    test

---
+++ dist/assets/Legal-CBN1poby.js
@@ -0,0 +1,21 @@
+import{c as n,r as s,j as i}from"./index-BhdGzFfQ.js";import{A as r,B as a}from"./AllRights-HqN6f-t2.js";import{D as l}from"./Description-CUZ-EfaP.js";const c=[{title:"General Info",description:`Welcome to Health Plus, your trusted online healthcare platform. Our
+          mission is to provide accessible and personalized healthcare services
+          to individuals seeking expert medical advice and treatment. By using
+          our platform, you agree to the terms outlined in our Privacy Policy
+          and Terms of Service.`},{title:"Privacy Policy",description:`Your privacy is paramount to us. Our Privacy Policy outlines how we
+          collect, use, and protect your personal and medical information. We
+          ensure secure data handling, and you can trust that your information
+          is treated with the utmost confidentiality.`},{title:"Terms of Service",description:`When using Health Plus, you agree to our Terms of Service. This
+          includes guidelines for using our platform, interacting with doctors,
+          and the responsibilities of both parties. It's essential to understand
+          these terms to ensure a smooth experience for all users.`},{title:"Consultations",description:`Our platform connects you with expert doctors who provide online
+          consultations. These consultations are not a replacement for in-person
+          medical visits but serve as a convenient option for medical advice,
+          prescriptions, and guidance. It's crucial to provide accurate and
+          complete information to receive the best possible care.`},{title:"How it Works",description:`Health Plus is designed to simplify healthcare access. You can choose
+          a specialist, schedule an appointment, and engage in a virtual
+          consultation. Our specialists offer personalized advice and treatment
+          plans tailored to your needs. Please remember that emergencies require
+          immediate medical attention and should be directed to your local
+          medical facility.`}];function d(){const e=n.c(2);s.useEffect(m);let t;e[0]===Symbol.for("react.memo_cache_sentinel")?(t=i.jsx("div",{className:"w-full block pb-2 bg-white text-center",children:i.jsx(a,{to:"/",classNameComponent:`text-[48px] tracking-[0.8px] font-semibold
+          `,classNamePlus:"text-[56px]"})}),e[0]=t):t=e[0];let o;return e[1]===Symbol.for("react.memo_cache_sentinel")?(o=i.jsxs(i.Fragment,{children:[t,i.jsx("ul",{className:"py-3 px-8 bg-gradient",children:c.map(u)}),i.jsx(r,{hasStyle:!0})]}),e[1]=o):o=e[1],o}function u(e){const{title:t,description:o}=e;return i.jsxs("li",{children:[i.jsx("h4",{className:"my-4 border-b-2 border-gray-500 text-[24px] xs:text-[32px] font-bold tracking-[0.8px]",children:t}),i.jsx(l,{variant:"legal",description:o})]},t)}function m(){window.scrollTo({top:0,behavior:"smooth"})}function x(){const e=n.c(1);let t;return e[0]===Symbol.for("react.memo_cache_sentinel")?(t=i.jsx(d,{}),e[0]=t):t=e[0],t}export{x as default};
